04729e8e6cf9f83a28889e79921a185aeb8fbcbb
corner_coder <56599360+fatihohn@users.noreply.github.com>
2025-11-03 03:33:29 +0900

    replace terraform with ec2 container reload

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -27,3 +27,8 @@ output "aws_region" {
   description = "The AWS region where resources are deployed."
   value       = var.aws_region
 }
+
+output "instance_ids" {
+  description = "The IDs of the deployed EC2 instances."
+  value       = concat(module.ec2_api.instance_ids, module.ec2_workers.instance_ids)
+}
